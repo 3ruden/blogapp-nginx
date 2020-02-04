8078f96a1bfce98c116418c0eb66f9917160f1c8
micchan <p1r2yy9knr@gmail.com>
2020-02-04 12:23:49 +0900

    unicornの設定を変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,22 +1,9 @@
-rails_root = File.expand_path('../../', __FILE__)
-
-# ワーカー数を指定します。
-# コア数と同じにすると良いようです。
+require 'fileutils'
+preload_app true
+timeout 5
 worker_processes 4
+listen '/tmp/nginx.socket', backlog: 1024
 
-# Railsアプリケーションのディレクトリを指定します。
-working_directory rails_root
-
-# 待ち受けるTCPポートを指定します。
-listen "#{rails_root}/tmp/unicorn.sock"
-
-# UnicornのPIDの一時保管場所を指定します。
-# 元々Railsには tmp/pids フォルダがあるのでそこに格納。
-pid "#{rails_root}/tmp/unicorn.pid"
-
-# 標準エラー出力のログファイルを指定します。
-# 元々Railsには log フォルダがあるのでそこに格納。
-stderr_path "#{rails_root}/log/unicorn_error.log"
-
-# 標準出力のログファイルを指定します。
-stdout_path "#{rails_root}/log/unicorn.log"
+before_fork do |server,worker|
+	FileUtils.touch('/tmp/app-initialized')
+end
